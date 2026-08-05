ee72e46687982b20f3dbdbce1bfded0054a7191f
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-08-05 13:16:41 +0200

    Update exportLabResults.R

--- R/exportLabResults.R
+++ R/exportLabResults.R
@@ -1,1 +1,29 @@
+exportLabResults <- function(
+    results,
+    outputFile
+) {
 
+  wb <- openxlsx::createWorkbook()
+
+  for (labName in names(results)) {
+
+    openxlsx::addWorksheet(
+      wb,
+      labName
+    )
+
+    openxlsx::writeData(
+      wb,
+      sheet = labName,
+      x = results[[labName]]
+    )
+
+  }
+
+  openxlsx::saveWorkbook(
+    wb,
+    file = outputFile,
+    overwrite = TRUE
+  )
+
+}
