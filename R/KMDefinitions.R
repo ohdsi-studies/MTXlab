3a97f85d2f375b8913df8e0737330a228fac606a
asaelmans <156896035+asaelmans@users.noreply.github.com>
2026-08-17 22:18:32 +0200

    Update KMDefinitions.R

--- R/KMDefinitions.R
+++ R/KMDefinitions.R
@@ -2,17 +2,27 @@
   condition = c(
     rep("Atopic dermatitis", 9),
     rep("Psoriasis", 9),
-    rep("Rheumatoid arthritis", 9)
+    rep("Rheumatoid arthritis", 9),
+    rep("Psoriatic arthritis", 9),
+    rep("Crohn's Disease", 9),
+    rep("Ulcerative Colitis", 9),
+    rep("Overall", 9)
   ),
   targetId = c(
     # AD
-    24843, 24844, 24845, 24846, 24847, 24848, 24849, 24850, 24851,
 
     # Psoriasis
-    25843, 25844, 25845, 25846, 25847, 25848, 25849, 25850, 25851,
 
     # RA
-    26843, 26844, 26845, 26846, 26847, 26848, 26849, 26850, 26851
+
+    #PsA
+
+    #Crohn's 
+
+    #Ulcerative colitis
+
+    #Overall
+    
   ),
   outcomeId = rep(
     c(
@@ -24,15 +34,15 @@
   ),
   laboratoryValue = rep(
     c(
-      "ALT (150 - 1000 U/L)",
-      "eGFR (10 - 45 mL/min/1.73m²)",
-      "Creatinine EU (125 - 500 μmol/L)",
-      "Creatinine USA (125 - 500 μmol/L)",
-      "Hemoglobin EU (2 - 6 mmol/L)",
-      "Hemoglobin USA (2 - 6 mmol/L)",
-      "Leucocytes EU (0.5 - 2.0 x 10⁹/L)",
-      "Leucocytes USA (0.5 - 2.0 x 10⁹/L)",
-      "Platelets (10 - 100 x 10⁹/L)"
+      "ALT (150 - 1000)",
+      "eGFR (10 - 45)",
+      "Creatinine EU (125 - 500)",
+      "Creatinine USA (125 - 500)",
+      "Hemoglobin EU (2 - 6)",
+      "Hemoglobin USA (2 - 6)",
+      "Leucocytes EU (0.5 - 2.0)",
+      "Leucocytes USA (0.5 - 2.0)",
+      "Platelets (10 - 100)"
     ),
     3
   )
